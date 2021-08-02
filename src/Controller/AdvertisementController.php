428597a79242d15713decf03b87c3bb5f778f886
nilsdacosta <nilsdacosta@gmail.com>
2021-08-02 15:22:15 +0200

    v1 homepageéé

--- src/Controller/AdvertisementController.php
+++ src/Controller/AdvertisementController.php
@@ -3,32 +3,92 @@
 namespace App\Controller;
 
 use App\Entity\Advertisement;
+use App\Form\AdvertisementType;
 use App\Repository\AdvertisementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Config\Loader\ParamConfigurator;
 
+/**
+ * @Route("/advertisement")
+ */
 class AdvertisementController extends AbstractController
 {
     /**
-     * @Route("/advertisements", name="advertisements")
+     * @Route("/", name="advertisement_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(AdvertisementRepository $advertisementRepository): Response
     {
         return $this->render('advertisement/index.html.twig', [
-            'controller_name' => 'AdvertisementController'
+            'advertisements' => $advertisementRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/advertisement/{id}", requirements={'id'= "\d+"}, name="show_advertisement")
+     * @Route("/new", name="advertisement_new", methods={"GET","POST"})
      */
-    public function advertisement(int $id, AdvertisementRepository $advertisementRepository): Response
+    public function new(Request $request): Response
     {
-        $advertisement = $advertisementRepository->findOneBy($id);
-        return $this->render('advertisement/show_advertisement.html.twig', [
-            'advertisement' => $advertisement
+        $advertisement = new Advertisement();
+        $form = $this->createForm(AdvertisementType::class, $advertisement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($advertisement);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('advertisement_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('advertisement/new.html.twig', [
+            'advertisement' => $advertisement,
+            'form' => $form,
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="advertisement_show", methods={"GET"})
+     */
+    public function show(Advertisement $advertisement): Response
+    {
+        return $this->render('advertisement/show.html.twig', [
+            'advertisement' => $advertisement,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="advertisement_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Advertisement $advertisement): Response
+    {
+        $form = $this->createForm(AdvertisementType::class, $advertisement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('advertisement_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('advertisement/edit.html.twig', [
+            'advertisement' => $advertisement,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="advertisement_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Advertisement $advertisement): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$advertisement->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($advertisement);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('advertisement_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/AdvertisementType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Advertisement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdvertisementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('updateDate')
+            ->add('isActive')
+            ->add('description')
+            ->add('announcer')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Advertisement::class,
+        ]);
+    }
+}
